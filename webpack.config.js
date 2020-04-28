6ff8a43272ac8e25af1eaa1afe06315a2ef7e477
Emily Olson <eolson@rightpoint.com>
2020-04-28 16:17:12 -0500

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const nodeExternals = require("webpack-node-externals");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackMd5Hash = require("webpack-md5-hash");
+
+module.exports = {
+  entry: {
+    main: ['./src/app.ts', './src/style.scss'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }, {
+        test: /\.s[ac]ss$/i,
+        use:  [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [ 
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+}
